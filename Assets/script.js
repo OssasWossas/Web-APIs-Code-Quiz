c0200ff03828deb3cff33ea3293889ed55871b4a
OssasWossas <muramba55@hotmail.com>
2021-06-27 17:41:58 +0800

    Added the  necessary file and am starting on the JS

---
+++ Assets/script.js
@@ -0,0 +1,52 @@
+const The_Questions = [
+    {
+        Question: "What color is the sky?",
+        answers: {
+            a: "Blue",
+            b: "Black",
+            c: "Eddie Murphey",
+            d: "Green"
+        },
+        actual_answer: "c"
+    },
+    {
+        Question: "Is Obama real?",
+        answers: {
+            a: "no",
+            b: "hell no",
+            c: "Dont look behind you",
+            d: "Donal Trump"
+        },
+        actual_answer: "c"
+    },
+    {
+        Question: "What color is the sky?",
+        answers: {
+            a: "Blue",
+            b: "Black",
+            c: "Eddie Murphey",
+            d: "Green"
+        },
+        actual_answer: "c"
+    },
+    {
+        Question: "What color is the sky?",
+        answers: {
+            a: "Blue",
+            b: "Black",
+            c: "Eddie Murphey",
+            d: "Green"
+        },
+        actual_answer: "c"
+    },
+    {
+        Question: "What color is the sky?",
+        answers: {
+            a: "Blue",
+            b: "Black",
+            c: "Eddie Murphey",
+            d: "Green"
+        },
+        actual_answer: "c"
+    },
+]
