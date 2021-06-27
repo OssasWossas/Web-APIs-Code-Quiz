0b4cbbd2224f99499f617fe7a3ed37419728690f
OssasWossas <muramba55@hotmail.com>
2021-06-27 20:58:05 +0800

    Got the start screen goin on and now going to ginish the quiz script

--- Assets/script.js
+++ Assets/script.js
@@ -1,52 +1,67 @@
+var quiz_div = document.getElementById("Quiz");
+var start_menu = document.getElementById("Start");
+var startbutton = document.getElementById("btn");
+var UL = document.getElementById("answers");
 const The_Questions = [
     {
         Question: "What color is the sky?",
         answers: {
-            a: "Blue",
-            b: "Black",
-            c: "Eddie Murphey",
-            d: "Green"
+            1: "Blue",
+            2: "Black",
+            3: "Eddie Murphey",
+            4: "Green"
         },
-        actual_answer: "c"
+        actual_answer: "3"
     },
     {
         Question: "Is Obama real?",
         answers: {
-            a: "no",
-            b: "hell no",
-            c: "Dont look behind you",
-            d: "Donal Trump"
+            1: "no",
+            2: "hell no",
+            3: "Dont look behind you",
+            4: "Donal Trump"
         },
-        actual_answer: "c"
+        actual_answer: "2"
     },
     {
-        Question: "What color is the sky?",
+        Question: "Whats the worst Framework?",
         answers: {
-            a: "Blue",
-            b: "Black",
-            c: "Eddie Murphey",
-            d: "Green"
+            1: "React",
+            2: "Vue",
+            3: "AngularJS a.k.a carrot farmer language",
+            4: "jQuery"
         },
-        actual_answer: "c"
+        actual_answer: "3"
     },
     {
-        Question: "What color is the sky?",
+        Question: "Have I run out of questions?",
         answers: {
-            a: "Blue",
-            b: "Black",
-            c: "Eddie Murphey",
-            d: "Green"
+            1: "No",
+            2: "Yes",
+            3: "Maybe",
+            4: "Who Cares"
         },
-        actual_answer: "c"
+        actual_answer: "1"
     },
     {
-        Question: "What color is the sky?",
+        Question: "Is tasmania Drifting away from Australia?",
         answers: {
-            a: "Blue",
-            b: "Black",
-            c: "Eddie Murphey",
-            d: "Green"
+            1: "Yes",
+            2: "Thankfully",
+            3: "No",
+            4: "Uganda"
         },
-        actual_answer: "c"
+        actual_answer: "1"
     },
-]
+];
+
+function init(){
+
+
+};
+
+startbutton.addEventListener('click', function () {
+    start_menu.style.visibility = "hidden";
+    quiz_div.style.visibility = "visible";
+    init()
+});
